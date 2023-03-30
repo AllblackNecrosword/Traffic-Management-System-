b43a8b09edd422151308f738e707617209fc6883
rabiiiii18 <np03cs4s220260@heraldcollege.edu.np>
2023-03-30 07:21:48 +0545

    updated main page

--- main.php
+++ main.php
@@ -5,9 +5,9 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/main.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="nav.css">
     <script src="https://kit.fontawesome.com/f0315fce63.js" crossorigin="anonymous"></script>
     <title>Nav</title>
 </head>
@@ -17,130 +17,125 @@
         <div class="main">
             <nav>
                 <!-- <h1>Logo</h1> -->
-                <img class="logo" src="images/logo.png">
+                <img class="logo" src="image/logo.png">
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Service</a></li>
                     <li><a href="#">About Us</a></li>
                     <!-- <li><a href="#">Contact Us</a></li> -->
-                    <li><a href="#">News</a></li>
+                    <li><a href="#">Notices</a></li>
                 </ul>
                 <a href="login.html" class="btn">Login</a>
             </nav>
         </div>
     </header>
-    <!-- <div id="carouselExampleCaptions" class="carousel slide">
+    <div id="carouselExampleIndicators" class="carousel slide">
         <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
                 aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
                 aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
                 aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="./image/traffic 1.jfif" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
+                <img class="coursel-img" src="image/Nepal-Go_Pro-Police-John-Rae-_87A6164.jpg" alt="">
             </div>
             <div class="carousel-item">
-                <img src="./image/Nepal-Go_Pro-Police-John-Rae-_87A6164.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
-                </div>
+                <img class="coursel-img" src="image/Nepal-Go_Pro-Police-John-Rae-_87A6164.jpg" alt="">
             </div>
             <div class="carousel-item">
-                <img src="./image/traffic 2.jpg" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
-                </div>
+                <img class="coursel-img" src="image/Nepal-Go_Pro-Police-John-Rae-_87A6164.jpg" alt="">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
             data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
             data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
-    </div> -->
-
+    </div>
     <div class="service_contain">
         <div class="title">
-            <span class="Service_title">Our Se</span>rvice
-
+            <span class="Service-title">Our Se</span>rvice
         </div>
-        <h5>All you need to know about our services</h5>
+        <h5 class="our_dec">All you need to know about our services</h5>
     </div>
 
     <div class="services">
         <div class="box">
             <i class="fa-solid fa-car"></i>
-            <h6> Vehicles<br>Regestration</h6>
+            <h6 class="box-heading"> Vehicles<br>Regestration</h6>
         </div>
         <div class="box">
             <i class="fa-solid fa-house"></i>
-            <h6> Lost Vehicles<br>Application</h6>
+            <h6 class="box-heading"> Lost Vehicles<br>Application</h6>
         </div>
         <div class="box">
             <i class="fa-solid fa-file"></i>
-            <h6> Lost Document<br>Application</h6>
+            <h6 class="box-heading"> Lost Document<br>Application</h6>
         </div>
         <div class="box">
             <i class="fa-solid fa-file"></i>
-            <h6> Lost Document<br>Application</h6>
+            <h6 class="box-heading"> Lost Document<br>Application</h6>
         </div>
         <div class="box">
             <i class="fa-solid fa-file"></i>
-            <h6> Lost Document<br>Application</h6>
+            <h6 class="box-heading"> Lost Document<br>Application</h6>
         </div>
         <div class="box">
             <i class="fa-solid fa-file"></i>
-            <h6> Lost Document<br>Application</h6>
+            <h6 class="box-heading"> Lost Document<br>Application</h6>
         </div>
         <div class="box">
             <i class="fa-solid fa-file"></i>
-            <h6> Lost Document<br>Application</h6>
+            <h6 class="box-heading"> Lost Document<br>Application</h6>
         </div>
         <div class="box">
             <i class="fa-solid fa-file"></i>
-            <h6> Lost Document<br>Application</h6>
+            <h6 class="box-heading"> Lost Document<br>Application</h6>
         </div>
     </div>
     <div class="aboutUs">
         <div class="service_contain">
             <div class="title">
-                <span class="Service_title">About </span>Us
-
+                <span class="Service-title">About</span> Us
             </div>
-            <h5>All you need to know about our services</h5>
+            <!-- <h5 class="our_dec">All you need to know about our services</h5> -->
         </div>
         <div class="about_content">
             <div class="text">
                 <p>
-                    Welcome to Stops, a website dedicated to providing information and solutions for traffic management
-                    systems. Our goal is to help reduce traffic congestion, promote safety on the roads, and improve
-                    overall
-                    transportation efficiency. We offer a range of resources, including real-time traffic updates,
-                    advice on
-                    route planning, and information on public transportation options. Our team is committed to making
-                    your
-                    commute as smooth and hassle-free as possible. Thank you for visiting Stops, and we hope you find
-                    our
-                    website helpful.</p>
+                    Welcome to Stops, your go-to destination for modern and efficient traffic management solutions. We
+                    are a team of passionate individuals dedicated to improving the safety and convenience of
+                    transportation for everyone.
+                    <br>
+                    <br>
+
+                    Our mission is to create innovative technologies and tools that empower transportation authorities,
+                    city planners, and individuals to manage traffic effectively. We believe that by leveraging
+                    cutting-edge technology and data-driven insights, we can make a meaningful impact on the way people
+                    move around our cities.
+                </p>
+                <div class="more-button">
+                    <a href="#" class="more-btn">See more</a>
+                </div>
+
+
             </div>
             <div class="about_img">
-                <!-- <img src="image/traffic 1.jfif" alt=""> -->
+                <div class="up-image">
+                    <img class="about_image" src="image/stopp.jfif" alt="">
+                </div>
+                <!-- <div class="down-image">
+                     <img class="about_image" src="image/stopp.jfif" alt="">
+                </div> -->
 
-                
             </div>
         </div>
     </div>
@@ -197,8 +192,8 @@
     <div class="copy-right">
         <p> STOPS &copy; 2023. All rights reserved.</p>
     </div>
-</body>
 
+</body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
     crossorigin="anonymous"></script>
